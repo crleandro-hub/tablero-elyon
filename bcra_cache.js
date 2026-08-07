f30b37aeffc121494f047e91f9a90aa473f2ee08
crleandro-hub <admgrupoelyon@gmail.com>
2026-08-07 08:30:08 -0300

    Actualizacion automatica de indicadores vie. 07/08/2026

--- bcra_cache.js
+++ bcra_cache.js
@@ -1,6 +1,6 @@
 /* -----------------------------------------------------------------
    bcra_cache.js  -  Grupo Elyon  |  Actualizado automaticamente
-   Generado: 2026-08-06T23:36:55   Fuente: API BCRA Estadisticas Monetarias v4.0
+   Generado: 2026-08-07T08:30:02   Fuente: API BCRA Estadisticas Monetarias v4.0
    tamar = serie 44 (TNA, bancos privados)
    badlar = serie 7 (TNA, bancos privados)
    uva = serie 31
@@ -8,6 +8,6 @@
 window.BCRA_CACHE = {
   tamar: { valor: 22.875, fecha: "2026-08-05" },
   badlar: { valor: 20.875, fecha: "2026-08-05" },
-  uva: { valor: 2065.5, fecha: "2026-08-06" },
-  updated: "2026-08-06T23:36:55"
+  uva: { valor: 2066.75, fecha: "2026-08-07" },
+  updated: "2026-08-07T08:30:02"
 };
